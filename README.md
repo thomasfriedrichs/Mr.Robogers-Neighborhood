b3f7c9d58cdb298ca1cabdb66c4d8dd67ce114b3
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-04-02 13:37:01 -0700

    add for loop, test1 passed

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Tests 
- * Describe: userNumber()
+ * Describe: returnNumbers()
  * Test: "It should return an array that stops at the number that the user puts in
  * code: userNumber(50);
  * expected output: 1-50
+
+ 
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-function userNumber(number) {
+function returnNumbers(usernumber) {
   let numbers = []
-  for (let i = 0; i <= number; i ++) {
-    return numbers.push(numbers[i])
-  }
+  for (let i = 0; i <= usernumber; i ++) {
+    numbers.push(i)
+    if (i === )
+  } 
+  return numbers
 }
