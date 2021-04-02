e9a9f5a1ec243f41b2d2eaf20878f210b637c1ae
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-04-02 16:02:32 -0700

    tweak function, still failing

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ function returnNumbers(usernumber) {
   let numbers = []
   for (let i = 0; i <= usernumber; i ++) {
     numbers.push(i)
-    if (i === 3) {
-      numbers[3] = "Won't you be my neighbor?"
-    } 
+    if (numbers[i].uncludes(3)) {
+      i[33] = "Won't you be my neighbor?"
+    }
   }  
   return numbers
 }
