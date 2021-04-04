c7c1004038da26082608b4732c04effed7733a1f
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-04-04 15:33:06 -0700

    add first conditional, passed test

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@ function returnNumbers(usernumber) {
   let numbers = []
   for (let i = 0; i <= usernumber; i ++) {
     numbers.push(i)
-    if (numbers[i].uncludes(3)) {
-      i[33] = "Won't you be my neighbor?"
+    if (numbers[i].toString().includes(3)) {
+      numbers[i] = "Won't you be my neighbor?"
+    } else if (numbers[i].toString().includes(2)) {
+      numbers[i] = ""
     }
   }  
   return numbers
